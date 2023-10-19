95df2aef684258b336e5ad40b2adf799d24f383e
xys1455 <111262124+xys1455@users.noreply.github.com>
2023-10-19 13:02:31 +0800

    Update script.js

--- geren/script.js
+++ geren/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".mutiple-text", {
-  strings: ["学生", "康老师粉丝", "编程爱好者", "..."],
+  strings: ["学生", "康老师粉丝", "编程爱好者", "音乐爱好者", "..."],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
